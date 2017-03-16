c9d04b89f02aa27802c51a7e4dffa94e2d4164b4
Hyman <2581788513@qq.com>
2017-03-16 16:03:55 +0800

    新版本2.2.3测试

--- app/src/main/java/com/example/compaq/nfc_student/OpenNFC.java
+++ app/src/main/java/com/example/compaq/nfc_student/OpenNFC.java
@@ -13,7 +13,7 @@ public class OpenNFC {
 	public OpenNFC(final Context c) {
 		new AlertDialog.Builder(c)
 				.setTitle("提示")
-				.setMessage("是否开启nfc?")
+				.setMessage("是否开启nfc呢?")
 				.setNegativeButton("是", new DialogInterface.OnClickListener() {
 
 					public void onClick(DialogInterface dialog, int which) {
